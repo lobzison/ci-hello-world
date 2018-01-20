d6ab8d187fdc56b321468b618d50738019cd8b4c
Глеб Лобов <lobzison@gmail.com>
2018-01-20 18:10:41 +0100

    added very complicated function

---
+++ ci-hello-world/ci_hello_world.py
@@ -0,0 +1,12 @@
+"""
+Simple module to test CI and test coverage
+"""
+import math
+
+
+def my_very_smart_func(num):
+    """
+    Doing some very complicated logic and algorithms,
+    returns sqr of num
+    """
+    return math.sqrt(num)
