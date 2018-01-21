e2f4792389f7fe3475036ffe907dbf0e146edce2
Глеб Лобов <lobzison@gmail.com>
2018-01-21 11:34:34 +0100

    added setup and packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CI Hello World',
+      version='1.0',
+      description='CI Hello World',
+      author='Gleb Lobov',
+      author_email='lobzison@gmail.com',
+      url='',
+      packages=['ci-hello-world', 'tests']
+      )
